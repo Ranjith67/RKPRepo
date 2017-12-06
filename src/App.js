7015d8403e769b9052a6654ea3a16205cbbf1515
Ranjith67 <30470134+Ranjith67@users.noreply.github.com>
2017-12-06 17:48:06 +0530

    Game app (#3)
    * Initial commit
    
    * first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+const Stars = (props) =>{
+return(
+<div>...</div>
+);
 }
 
-export default App;
+const Button = (props) =>{
+return(
+    <div>...</div>
+);
+}
+
+const Answer = (props) =>{
+return(
+    <div>...</div>
+);
+}
+
+class Game extends React.Component{
+
+    render(){
+        return(
+        <div>
+            <h3>Play Nine!</h3>
+            <Stars />
+        </div>
+        );
+    }
+}
+
+class App extends React.Component{
+    render(){
+        return(
+           <div><Game /></div> 
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
